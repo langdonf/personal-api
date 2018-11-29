62a9265089f592c0e641beaa21cefb3002c8b860
langdonf <lfroker@gmail.com>
2018-11-29 11:22:07 -0800

    addfixed broken link

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
-const db = require('/models')
+const db = require('./models')
 // MIDDLEWARE
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json())
